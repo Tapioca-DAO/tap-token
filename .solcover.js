f9fd745918c4f356cd00a75eb51d0cce9c1f25f5
cryptotechmaker <grigore.cosmin@gmail.com>
2022-09-16 15:34:27 +0300

    feat: added omniAura flow

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,3 @@
 module.exports = {
-    skipFiles: [`vyper/mocks`, 'tokens/mocks', 'tokens/OFT20', 'tokens/TapToken.sol', 'Minter.sol'],
+    skipFiles: [`vyper/mocks`, 'tokens/mocks', 'tokens/OFT20', 'tokens/TapToken.sol', 'Minter.sol', 'aura/mocks'],
 };
