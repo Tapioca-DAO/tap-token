198c0440efa5d8e3f2b14f3d63bddae39a5e75c0
0xRektora <13756173+0xRektora@users.noreply.github.com>
2022-12-11 11:15:25 -0500

    patch: `oTAP.exercise()`

--- contracts/options/oTAP.sol
+++ contracts/options/oTAP.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 //
 //                 .(%%%%%%%%%%%%*       *
@@ -28,17 +29,20 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 struct TapOption {
     uint128 expiry; // timestamp, as once one wise man said, the sun will go dark before this overflows
     uint128 amount; // amount of entitled TAP. Tap has a max supply of 100*10^24, so this should be fine
+    bool exercised;
 }
 
 contract OTAP is ERC721 {
+    address public immutable minter; // address of the minter
+    IERC20 public immutable TAP; // address of the TAP token
     uint256 public mintedOTAP; // total number of OTAP minted
     uint256 public mintedTAP; // total number of TAP minted
-    address public immutable minter; // address of the minter
 
     mapping(uint256 => TapOption) public options; // tokenId => Option
 
-    constructor(address _minter) ERC721('Option TAP', 'oTAP') {
+    constructor(address _minter, address _TAP) ERC721('Option TAP', 'oTAP') {
         minter = _minter;
+        TAP = IERC20(_TAP);
     }
 
     modifier onlyMinter() {
@@ -93,9 +97,13 @@ contract OTAP is ERC721 {
     function exercise(uint256 _tokenId) external onlyMinter {
         TapOption memory option = options[_tokenId];
         require(option.expiry >= block.timestamp, 'OTAP: option expired');
+        require(!option.exercised, 'OTAP: option already exercised');
+        address owner = ownerOf(_tokenId);
 
         mintedTAP += uint256(option.amount);
+        options[_tokenId].exercised = true;
+        TAP.transfer(owner, uint256(option.amount));
 
-        emit Exercise(ownerOf(_tokenId), _tokenId);
+        emit Exercise(owner, _tokenId);
     }
 }
