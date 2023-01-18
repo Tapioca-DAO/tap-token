6ddcc8a9de6e2ef39a69fd6df9faab6eb181c6d4
cryptotechmaker <grigore.cosmin@gmail.com>
2023-01-18 16:32:51 +0200

    updated tapioca-sdk

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -67,22 +67,12 @@ const config: HardhatUserConfig & { vyper: any; dodoc: any } = {
             },
         },
         //testnets
-        arbitrum_goerli: {
-            url: `https://arb-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-            accounts: [process.env.PRIVATE_KEY!],
-            chainId: 421613,
-            lzChainId: '10143',
-        },
-        mumbai: {
-            url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-            accounts: [process.env.PRIVATE_KEY!],
-        },
         goerli: supportedChains['goerli'],
         bnb_testnet: supportedChains['bnb_testnet'],
         fuji_avalanche: supportedChains['fuji_avalanche'],
-        //mumbai: supportedChains['mumbai'],
+        mumbai: supportedChains['mumbai'],
         fantom_testnet: supportedChains['fantom_testnet'],
-        // arbitrum_goerli: supportedChains['arbitrum_goerli'],
+        arbitrum_goerli: supportedChains['arbitrum_goerli'],
         optimism_goerli: supportedChains['optimism_goerli'],
         harmony_testnet: supportedChains['harmony_testnet'],
 
