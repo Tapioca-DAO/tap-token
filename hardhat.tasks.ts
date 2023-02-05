93c4a68004685997b05e477f809bdac5c57aaf29
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 13:22:19 -0500

    patch: Updated tasks

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -2,8 +2,9 @@ import '@nomiclabs/hardhat-ethers';
 import { task } from 'hardhat/config';
 import { exportSDK__task } from './tasks/exportSDK';
 import { setTrustedRemote__task } from './tasks/setTrustedRemote';
-import { deployERC20Mock__task, deployVesting__task } from './tasks/contractDeployment';
+import { deployERC20Mock__task, deployOracleMock__task, deployVesting__task } from './tasks/contractDeployment';
 import { setOracleMockRate__task } from './tasks/setterTasks';
+import { getLocalDeployments__task, getSDKDeployments__task } from './tasks/getDeployments';
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
@@ -18,6 +19,15 @@ task('exportSDK', 'Generate and export the typings and/or addresses for the SDK.
     'Using the current chain ID deployments.',
 );
 
+task('getLocalDeployment', 'Try to load a local deployment.', getLocalDeployments__task).addOptionalParam(
+    'contractName',
+    'The name of the contract to load',
+);
+
+task('getSDKDeployment', 'Try to load an SDK deployment.', getSDKDeployments__task)
+    .addParam('repo', 'The name of the repo to load the deployment from')
+    .addOptionalParam('contractName', 'The name of the contract to load');
+
 task('setTrustedRemote', 'Calls setTrustedRemote on TapOFT contract', setTrustedRemote__task)
     .addParam('chain', 'LZ destination chain id for trusted remotes')
     .addParam('dst', 'TapOFT destination address')
@@ -29,10 +39,14 @@ task('deployVesting', 'Deploys a new Vesting contract', deployVesting__task)
     .addParam('cliff', 'Cliff duration in seconds')
     .addParam('duration', 'Vesting duration in seconds');
 
-task('deployERC20Mock', 'Deploys a new Vesting contract', deployERC20Mock__task)
+task('deployERC20Mock', 'Deploys a new ERC20 Mock contract', deployERC20Mock__task)
     .addParam('deploymentName', 'The name of the deployment')
     .addParam('initialAmount', 'Initial amount of tokens')
     .addParam('decimals', 'Number of decimals');
+
+task('deployOracleMock', 'Deploys a new Oracle mock contract', deployOracleMock__task)
+    .addParam('deploymentName', 'The name of the deployment')
+    .addParam('erc20Name', 'Initial amount of tokens');
 
 task('setOracleMockRate', 'Set exchange rate for a mock oracle', setOracleMockRate__task)
     .addParam('oracleAddress', 'Address of the oracle')
