eda1a71f9afea9e3afbdbe9c2d97258881d5041a
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 13:45:12 -0500

    patch: Updated `exportSDK`

--- tasks/exportSDK.ts
+++ tasks/exportSDK.ts
@@ -1,55 +1,41 @@
 import fs from 'fs';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import SDK from 'tapioca-sdk';
+import { TProjectDeployment } from 'tapioca-sdk/dist/shared';
 import { glob, runTypeChain } from 'typechain';
 import writeJsonFile from 'write-json-file';
 import { getDeployments } from '../scripts/getDeployments-script';
+import { getLocalDeployments__task } from './getDeployments';
 /**
  * Script used to generate typings for the tapioca-sdk
  * https://github.com/Tapioca-DAO/tapioca-sdk
  */
 
 export const exportSDK__task = async (taskArgs: { mainnet?: boolean }, hre: HardhatRuntimeEnvironment) => {
-    const cwd = process.cwd();
-    const { mainnet } = taskArgs;
-    const __deployments = { prev: {} };
-    try {
-        __deployments.prev = JSON.parse(fs.readFileSync('tapioca-sdk/src/addresses.json', 'utf-8'));
-    } catch (e) {}
-    if (mainnet) {
-        const deployments = {
-            ...__deployments.prev,
-            [await hre.getChainId()]: await getDeployments(hre),
-        };
-        await writeJsonFile('tapioca-sdk/src/addresses.json', deployments);
-    }
-    // We are looking at
-    // BeachBar, Mixologist, MixologistHelper;
-    const allFiles = glob(cwd, [`${hre.config.paths.artifacts}/**/!(*.dbg).json`]).filter((e) =>
-        [
-            'OFT20',
-            'TapOFT',
-            'ERC20Mock',
-            'LzEndpointMock',
-            'FeeDistributor',
-            'GaugeController',
-            'LiquidityGauge',
-            'Minter',
-            'VeTap',
-            'IFeeDistributor',
-            'IGaugeController',
-            'ILiquidityGauge',
-        ].some((v) => e.split('/').slice(-1)[0] === v.concat('.json')),
-    );
-    await runTypeChain({
-        cwd,
-        filesToProcess: allFiles,
-        allFiles,
-        outDir: 'tapioca-sdk/src/typechain/tap-token',
-        target: 'ethers-v5',
-        flags: {
-            alwaysGenerateOverloads: true,
-            environment: 'hardhat',
-            discriminateTypes: false,
-        },
+    const chainId = await hre.getChainId();
+
+    const _deployments: TProjectDeployment = {
+        [chainId as keyof TProjectDeployment]: SDK.API.utils.getDeployments('Tap-Token', chainId, true) ?? [],
+    };
+
+    const contractNames = [
+        'OFT20',
+        'TapOFT',
+        'ERC20Mock',
+        'OracleMock',
+        'OTAP',
+        'TapiocaOptionBroker',
+        'TapiocaOptionLiquidityProvision.json',
+        'Vesting',
+    ];
+
+    console.log('[+] Exporting typechain & deployment files for tapioca-sdk...');
+    console.log(contractNames);
+
+    await SDK.API.exportSDK.run({
+        artifactPath: hre.config.paths.artifacts,
+        projectCaller: 'Tap-Token',
+        contractNames,
+        _deployments,
     });
 };
