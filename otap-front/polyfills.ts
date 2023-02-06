fb295922cd515de92eea151835e3caf4f779a05c
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 20:56:12 -0500

    frontend: linting

--- otap-front/polyfills.ts
+++ otap-front/polyfills.ts
@@ -1,6 +1,6 @@
-import { Buffer } from 'buffer'
-import process from 'process'
+import { Buffer } from 'buffer';
+import process from 'process';
 
-window.global = window
-window.process = process
-window.Buffer = Buffer
+window.global = window;
+window.process = process;
+window.Buffer = Buffer;
--- otap-front/vite.config.ts
+++ otap-front/vite.config.ts
@@ -1,16 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      util: 'util',
+    define: {
+        global: 'globalThis',
     },
-  },
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            process: 'process/browser',
+            util: 'util',
+        },
+    },
+    plugins: [react()],
+});
