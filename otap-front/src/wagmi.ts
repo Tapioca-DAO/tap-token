e59d96e005c856670eed8aaeb3be02c803a7368a
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 23:30:27 -0500

    frontend: Generated contract hooks

--- otap-front/src/wagmi.ts
+++ otap-front/src/wagmi.ts
@@ -1,9 +1,25 @@
 import { getDefaultClient } from 'connectkit';
-import { createClient } from 'wagmi';
+import { Chain, createClient } from 'wagmi';
 
+const GOERLI_CHAIN: Chain = {
+    id: 5,
+    name: 'Goerli',
+    nativeCurrency: {
+        decimals: 18,
+        name: 'Goerli Ether',
+        symbol: 'GoETH',
+    },
+    network: 'Goerli',
+    rpcUrls: {
+        default: { http: ['https://eth-goerli.g.alchemy.com/v2/631U-TWNMURg0u4lIqrjat0LraWguV6p'] },
+        public: { http: ['https://eth-goerli.alchemyapi.io/v2/631U-TWNMURg0u4lIqrjat0LraWguV6p'] },
+    },
+    testnet: true,
+};
 export const client = createClient(
     getDefaultClient({
         autoConnect: true,
-        appName: 'My wagmi + ConnectKit App',
+        appName: 'oTAP prototype',
+        chains: [GOERLI_CHAIN],
     }),
 );
