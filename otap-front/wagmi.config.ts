01d05066709e0cbb101721c92230756b8ec21132
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 20:56:05 -0500

    frontend: wagmi config setup

--- otap-front/wagmi.config.ts
+++ otap-front/wagmi.config.ts
@@ -1,15 +1,26 @@
 import { defineConfig } from '@wagmi/cli';
-import { hardhat } from '@wagmi/cli/plugins';
+import { hardhat, react } from '@wagmi/cli/plugins';
 
 export default defineConfig({
     out: 'src/generated.ts',
     contracts: [],
     plugins: [
+        react(),
         hardhat({
             project: '../../tap-token',
             commands: {
                 build: 'npx hardhat compile',
             },
+            include: [
+                'OFT20',
+                'TapOFT',
+                'ERC20Mock',
+                'OracleMock',
+                'OTAP',
+                'TapiocaOptionBroker',
+                'TapiocaOptionLiquidityProvision',
+                'Vesting',
+            ].map((e) => `${e}.json`),
         }),
     ],
 });
