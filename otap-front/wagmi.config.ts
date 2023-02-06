c3ea6044a5a4b9085fc4e89928f96d93f2323621
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-02-05 19:23:23 -0500

    init: `otap-frontend`

---
+++ otap-front/wagmi.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@wagmi/cli';
+import { hardhat } from '@wagmi/cli/plugins';
+
+export default defineConfig({
+    out: 'src/generated.ts',
+    contracts: [],
+    plugins: [
+        hardhat({
+            project: '../../tap-token',
+            commands: {
+                build: 'npx hardhat compile',
+            },
+        }),
+    ],
+});
