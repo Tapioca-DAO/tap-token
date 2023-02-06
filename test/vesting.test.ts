86de571c63bd55af61c922e3a014ba758d8e2d40
cryptotechmaker <grigore.cosmin@gmail.com>
2023-02-06 09:59:01 +0200

    Vesting contract required changes

--- test/vesting.test.ts
+++ test/vesting.test.ts
@@ -5,7 +5,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { ERC20Mock } from '../typechain';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
-describe('Vesting', () => {
+describe.only('Vesting', () => {
     let usdc: ERC20Mock;
     let deployer: SignerWithAddress;
     let eoa1: SignerWithAddress;
@@ -25,13 +25,13 @@ describe('Vesting', () => {
         const cliff = 86400 * 10;
         const duration = 86400 * 100;
         const { vesting } = await registerVesting(usdc.address, cliff, duration);
+        await usdc.transfer(vesting.address, mintAmount);
 
-        await usdc.approve(vesting.address, mintAmount);
-        await expect(vesting.connect(eoa1).init(mintAmount)).to.be.revertedWith('Ownable: caller is not the owner');
-        await expect(vesting.init(0)).to.be.revertedWith('Vesting: no tokens');
+        await expect(vesting.connect(eoa1).init(usdc.address, mintAmount)).to.be.revertedWith('Ownable: caller is not the owner');
+        await expect(vesting.init(usdc.address, 0)).to.be.revertedWithCustomError(vesting, 'NoTokens');
 
-        await vesting.init(mintAmount);
-        await expect(vesting.init(mintAmount)).to.be.revertedWith('Vesting: initialized');
+        await vesting.init(usdc.address, mintAmount);
+        await expect(vesting.init(usdc.address, mintAmount)).to.be.revertedWithCustomError(vesting, 'Initialized');
 
         const savedCliff = await vesting.cliff();
         expect(savedCliff.eq(cliff)).to.be.true;
@@ -70,17 +70,19 @@ describe('Vesting', () => {
         const cliff = 86400 * 10;
         const duration = 86400 * 100;
         const { vesting } = await registerVesting(usdc.address, cliff, duration);
-
-        await usdc.approve(vesting.address, mintAmount);
+        await usdc.transfer(vesting.address, mintAmount);
 
         await vesting.registerUser(eoa1.address, mintAmount.div(10));
         await vesting.registerUser(deployer.address, mintAmount.div(5));
-        await expect(vesting.registerUser(deployer.address, mintAmount.div(20))).to.be.revertedWith('Vesting: user registered');
+        await expect(vesting.registerUser(deployer.address, mintAmount.div(20))).to.be.revertedWithCustomError(
+            vesting,
+            'AlreadyRegistered',
+        );
 
-        await expect(vesting.init(mintAmount.div(100))).to.be.revertedWith('Vesting: not enough');
-        await vesting.init(mintAmount);
+        await expect(vesting.init(usdc.address, mintAmount.div(100))).to.be.revertedWithCustomError(vesting, 'NotEnough');
+        await vesting.init(usdc.address, mintAmount);
 
-        await expect(vesting.registerUser(eoa1.address, mintAmount.div(5))).to.be.revertedWith('Vesting: initialized');
+        await expect(vesting.registerUser(eoa1.address, mintAmount.div(5))).to.be.revertedWithCustomError(vesting, 'Initialized');
 
         let userInfo = await vesting.users(deployer.address);
         expect(userInfo[0].gt(0)).to.be.true;
@@ -99,7 +101,8 @@ describe('Vesting', () => {
         const cliff = 86400 * 10;
         const duration = 86400 * 100;
         const { vesting } = await registerVesting(usdc.address, cliff, duration);
-        await usdc.approve(vesting.address, mintAmount);
+        await usdc.transfer(vesting.address, mintAmount);
+
         const newSigners = await randomSigners(5);
 
         await vesting.registerUser(eoa1.address, mintAmount.div(4));
@@ -108,7 +111,7 @@ describe('Vesting', () => {
             await vesting.registerUser(newSigners[i].address, mintAmount.div(10));
         }
 
-        await vesting.init(mintAmount);
+        await vesting.init(usdc.address, mintAmount);
         const totalAmount = await vesting.seeded();
         expect(totalAmount.eq(mintAmount)).to.be.true;
 
@@ -156,10 +159,10 @@ describe('Vesting', () => {
 
         await time_travel(duration);
 
-        await expect(vesting.claim()).to.be.revertedWith('Vesting: nothing');
-        await expect(vesting.connect(eoa1).claim()).to.be.revertedWith('Vesting: nothing');
+        await expect(vesting.claim()).to.be.revertedWithCustomError(vesting, 'NothingToClaim');
+        await expect(vesting.connect(eoa1).claim()).to.be.revertedWithCustomError(vesting, 'NothingToClaim');
         for (var i = 0; i < newSigners.length; i++) {
-            await expect(vesting.connect(newSigners[i]).claim()).to.be.revertedWith('Vesting: nothing');
+            await expect(vesting.connect(newSigners[i]).claim()).to.be.revertedWithCustomError(vesting, 'NothingToClaim');
         }
     });
 
@@ -170,7 +173,8 @@ describe('Vesting', () => {
         const cliff = 86400 * 10;
         const duration = 86400 * 100;
         const { vesting } = await registerVesting(usdc.address, cliff, duration);
-        await usdc.approve(vesting.address, mintAmount);
+        await usdc.transfer(vesting.address, mintAmount);
+
         const newSigners = await randomSigners(5);
 
         await vesting.registerUser(eoa1.address, mintAmount.div(4));
@@ -179,7 +183,7 @@ describe('Vesting', () => {
             await vesting.registerUser(newSigners[i].address, mintAmount.div(10));
         }
 
-        await vesting.init(mintAmount);
+        await vesting.init(usdc.address, mintAmount);
 
         const totalAmount = await vesting.seeded();
         expect(totalAmount.eq(mintAmount)).to.be.true;
@@ -191,160 +195,5 @@ describe('Vesting', () => {
 
         expect(totalVesting.eq(totalClaimable)).to.be.true;
         expect(totalVesting.eq(mintAmount)).to.be.true;
-    });
-
-    it('should test general revoke', async () => {
-        const mintAmount = ethers.BigNumber.from((1e18).toString()).mul(1000);
-        await usdc.freeMint(mintAmount);
-
-        const cliff = 86400 * 10;
-        const duration = 86400 * 100;
-        const { vesting } = await registerVesting(usdc.address, cliff, duration);
-        await usdc.approve(vesting.address, mintAmount);
-        const newSigners = await randomSigners(5);
-
-        await vesting.registerUser(eoa1.address, mintAmount.div(4));
-        await vesting.registerUser(deployer.address, mintAmount.div(4));
-        for (var i = 0; i < newSigners.length; i++) {
-            await vesting.registerUser(newSigners[i].address, mintAmount.div(10));
-        }
-
-        await vesting.init(mintAmount);
-
-        await expect(vesting.connect(eoa1)['emergencyRevoke()']()).to.be.reverted;
-        await expect(vesting['emergencyRevoke()']()).to.be.revertedWith('Vesting: not requested');
-
-        const vestingBalanceBefore = await usdc.balanceOf(vesting.address);
-        expect(vestingBalanceBefore.eq(mintAmount)).to.be.true;
-
-        let requestedAt = await vesting.revokeRequestedAt();
-        expect(requestedAt.eq(0)).to.be.true;
-
-        await expect(vesting.connect(eoa1)['requestEmergencyRevoke()']()).to.be.reverted;
-        await vesting['requestEmergencyRevoke()']();
-
-        requestedAt = await vesting.revokeRequestedAt();
-        expect(requestedAt.gt(0)).to.be.true;
-
-        await expect(vesting['requestEmergencyRevoke()']()).to.be.revertedWith('Vesting: requested');
-
-        await expect(vesting['emergencyRevoke()']()).to.be.revertedWith('Vesting: too early');
-
-        await time_travel(86401);
-
-        await vesting['emergencyRevoke()']();
-
-        const vestingBalanceAfter = await usdc.balanceOf(vesting.address);
-        expect(vestingBalanceAfter.eq(0)).to.be.true;
-        const ownerUsdcBalance = await usdc.balanceOf(deployer.address);
-        expect(ownerUsdcBalance.eq(mintAmount)).to.be.true;
-
-        await expect(vesting['emergencyRevoke()']()).to.be.revertedWith('Vesting: revoked');
-
-        const newRevokedAt = await vesting.revokeRequestedAt();
-        expect(newRevokedAt.eq(0)).to.be.true;
-
-        const revokedStatus = await vesting.revoked();
-        expect(revokedStatus).to.be.true;
-    });
-
-    it('should test user revoke', async () => {
-        const mintAmount = ethers.BigNumber.from((1e18).toString()).mul(1000);
-        await usdc.freeMint(mintAmount);
-
-        const cliff = 86400 * 10;
-        const duration = 86400 * 100;
-        const { vesting } = await registerVesting(usdc.address, cliff, duration);
-        await usdc.approve(vesting.address, mintAmount);
-        const newSigners = await randomSigners(5);
-
-        await vesting.registerUser(eoa1.address, mintAmount.div(4));
-        await vesting.registerUser(deployer.address, mintAmount.div(4));
-        for (var i = 0; i < newSigners.length; i++) {
-            await vesting.registerUser(newSigners[i].address, mintAmount.div(10));
-        }
-
-        await vesting.init(mintAmount);
-
-        await time_travel(duration / 2);
-        await vesting.connect(eoa1).claim();
-
-        const usdcBalanceBefore = await usdc.balanceOf(deployer.address);
-        let userInfoBefore = await vesting.users(eoa1.address);
-
-        const revokedStatusBefore = userInfoBefore[3];
-        expect(revokedStatusBefore).to.be.false;
-        const claimed = userInfoBefore[1];
-        expect(claimed.gt(0)).to.be.true;
-        const amount = userInfoBefore[0];
-        expect(amount.eq(mintAmount.div(4))).to.be.true;
-
-        let requestedAt = await vesting.revokeRequestedAt();
-        expect(requestedAt.eq(0)).to.be.true;
-
-        await expect(vesting.connect(eoa1)['requestEmergencyRevoke(address)'](eoa1.address)).to.be.reverted;
-        await vesting['requestEmergencyRevoke(address)'](eoa1.address);
-
-        requestedAt = await vesting.revokeRequestedAt();
-        expect(requestedAt.gt(0)).to.be.true;
-
-        await expect(vesting['requestEmergencyRevoke(address)'](eoa1.address)).to.be.revertedWith('Vesting: requested');
-
-        await expect(vesting['emergencyRevoke(address)'](eoa1.address)).to.be.revertedWith('Vesting: too early');
-
-        await time_travel(86401);
-        await vesting['emergencyRevoke(address)'](eoa1.address);
-
-        await expect(vesting['emergencyRevoke(address)'](eoa1.address)).to.be.revertedWith('Vesting: revoked');
-
-        const newRevokedAt = await vesting.revokeRequestedAt();
-        expect(newRevokedAt.eq(0)).to.be.true;
-
-        userInfoBefore = await vesting.users(eoa1.address);
-
-        const revokedStatusAfter = userInfoBefore[3];
-        expect(revokedStatusAfter).to.be.true;
-        const claimedAfter = userInfoBefore[1];
-        expect(claimedAfter.eq(claimed)).to.be.true;
-        const amountAfter = userInfoBefore[0];
-        expect(amountAfter.eq(mintAmount.div(4))).to.be.true;
-        const usdcBalanceAfter = await usdc.balanceOf(deployer.address);
-        expect(usdcBalanceAfter.sub(usdcBalanceBefore).eq(amount.sub(claimed))).to.be.true;
-
-        const signer0BalanceBefore = await usdc.balanceOf(newSigners[0].address);
-        await vesting.connect(newSigners[0]).claim();
-        const signer0BalanceAfter = await usdc.balanceOf(newSigners[0].address);
-
-        expect(signer0BalanceBefore.eq(0)).to.be.true;
-        expect(signer0BalanceAfter.gte(mintAmount.div(21))).to.be.true;
-    });
-
-    it('should test pause functionality', async () => {
-        const mintAmount = ethers.BigNumber.from((1e18).toString()).mul(1000);
-        await usdc.freeMint(mintAmount);
-
-        const cliff = 86400 * 10;
-        const duration = 86400 * 100;
-        const { vesting } = await registerVesting(usdc.address, cliff, duration);
-        await usdc.approve(vesting.address, mintAmount);
-        const newSigners = await randomSigners(5);
-
-        await vesting.registerUser(eoa1.address, mintAmount.div(4));
-        await vesting.registerUser(deployer.address, mintAmount.div(4));
-        for (var i = 0; i < newSigners.length; i++) {
-            await vesting.registerUser(newSigners[i].address, mintAmount.div(10));
-        }
-
-        await vesting.init(mintAmount);
-
-        let pauseState = await vesting.paused();
-        expect(pauseState).to.be.false;
-
-        await expect(vesting.connect(eoa1).updatePause(true)).to.be.revertedWith('Vesting: unauthorized');
-        await vesting.updatePause(true);
-        pauseState = await vesting.paused();
-        expect(pauseState).to.be.true;
-
-        await expect(vesting.connect(eoa1).claim()).to.be.revertedWith(`Vesting: paused`);
     });
 });
