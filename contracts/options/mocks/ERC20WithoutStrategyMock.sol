c875c073749cc78241051f9c94065f60b5dc1a3b
cryptotechmaker <grigore.cosmin@gmail.com>
2023-02-13 16:26:58 +0200

    fixed tests

---
+++ contracts/options/mocks/ERC20WithoutStrategyMock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'tapioca-sdk/dist/contracts/YieldBox/contracts/strategies/ERC20WithoutStrategy.sol';
+
+// solhint-disable const-name-snakecase
+// solhint-disable no-empty-blocks
+
+contract ERC20WithoutStrategyMock is ERC20WithoutStrategy {
+    using BoringERC20 for IERC20;
+
+    constructor(IYieldBox _yieldBox, IERC20 tokn) ERC20WithoutStrategy(_yieldBox, tokn) {}
+}
