4821eea8940c371025b0162823ffd864e7ff1ab7
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-03-16 16:46:21 -0400

    patch: Small changes

--- tasks/deploy/99-buildTestnetAfterDepSetup.ts
+++ tasks/deploy/99-buildTestnetAfterDepSetup.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { TDeploymentVMContract } from 'tapioca-sdk/dist/ethers/hardhat/DeployerVM';
-import { Multicall3 } from '../../typechain';
+import { Multicall3 } from 'tapioca-sdk/dist/typechain/utils/MultiCall';
 
 export const buildTestnetAfterDepSetup = async (
     hre: HardhatRuntimeEnvironment,
--- tasks/deployStack.ts
+++ tasks/deployStack.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { Multicall3 } from 'tapioca-sdk/dist/typechain/utils/MultiCall';
-import { Multicall3__factory } from '../typechain';
 import { buildTapOFT } from './deploy/01-buildTapOFT';
 import { buildTOLP } from './deploy/02-buildTOLP';
 import { buildOTAP } from './deploy/03-buildOTAP';
@@ -9,6 +8,7 @@ import { buildAfterDepSetup } from './deploy/05-buildAfterDepSetup';
 import { buildYieldBoxMock } from './deploy/901-buildYieldBoxMock';
 import { buildTestnetDeployment } from './deploy/902-buildTestnetDeployment';
 import { buildTestnetAfterDepSetup } from './deploy/99-buildTestnetAfterDepSetup';
+import { typechain } from 'tapioca-sdk';
 
 // hh deployStack --type build --network goerli
 export const deployStack__task = async (
@@ -17,14 +17,15 @@ export const deployStack__task = async (
 ) => {
     // Settings
     const signer = (await hre.ethers.getSigners())[0];
+    const multicall = typechain.Multicall.Multicall3__factory.connect(
+        hre.SDK.config.MULTICALL_ADDRESS,
+        signer,
+    );
     const VM = new hre.SDK.DeployerVM(hre, {
         // Change this if you get bytecode size error / gas required exceeds allowance (550000000)/ anything related to bytecode size
         // Could be different by network/RPC provider
-        bytecodeSizeLimit: 100_000,
-        multicall: Multicall3__factory.connect(
-            hre.SDK.config.MULTICALL_ADDRESS,
-            signer,
-        ),
+        bytecodeSizeLimit: 95_000,
+        multicall,
     });
 
     if (taskArgs.type === 'build') {
@@ -61,10 +62,6 @@ export const deployStack__task = async (
 
     const vmList = VM.list();
     // After deployment setup
-    const multiCall = await hre.ethers.getContractAt(
-        'Multicall3',
-        hre.SDK.config.MULTICALL_ADDRESS,
-    );
     const calls: Multicall3.Call3Struct[] = [
         ...(await buildAfterDepSetup(hre, vmList)),
     ];
@@ -76,7 +73,7 @@ export const deployStack__task = async (
     // Execute
     console.log('[+] After deployment setup calls number: ', calls.length);
     try {
-        const tx = await (await multiCall.aggregate3(calls)).wait(1);
+        const tx = await (await multicall.aggregate3(calls)).wait(1);
         console.log(
             '[+] After deployment setup multicall Tx: ',
             tx.transactionHash,
