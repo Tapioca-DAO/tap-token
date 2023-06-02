cbf62df1808ebdd669214839dca1ba6f4d859fd4
Barry Lyndon <redmond@barrylyndon.dev>
2023-06-02 22:33:37 +0200

    Add .prettierrc that matches linter options

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  overrides: [
+    {
+      files: "*.ts",
+      options: {
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 80,
+        endOfLine: 'auto',
+        useTabs: false,
+        tabWidth: 4,
+      },
+    },
+  ],
+};
