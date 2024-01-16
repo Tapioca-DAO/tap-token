d52c176a76c3191a1baa1e563425fc45ae7b0f18
0xWeiss <mweiss.eth@gmail.com>
2024-01-17 03:57:41 +0400

    WIP: Updated mock Oracle

--- test/Mocks/TapOracleMock.sol
+++ test/Mocks/TapOracleMock.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.22;
 
 
-import {TapOracle} from "tapioca-periph/contracts/oracle/implementations/Arbitrum/TapOracle.sol";
 import {IOracle} from "tapioca-periph/contracts/interfaces/IOracle.sol";
 
 
@@ -10,10 +9,20 @@ contract TapOracleMock is IOracle{
 
     constructor() {}
 
-
+    function decimals() external view returns (uint8){}
     function get(
         bytes calldata data
     ) external returns (bool success, uint256 rate){
         return (true, 1);
     }
+
+        function peek(
+        bytes calldata data
+    ) external view returns (bool success, uint256 rate){}
+
+    function peekSpot(bytes calldata data) external view returns (uint256 rate){}
+
+    function symbol(bytes calldata data) external view returns (string memory){}
+
+    function name(bytes calldata data) external view returns (string memory){}
 }
