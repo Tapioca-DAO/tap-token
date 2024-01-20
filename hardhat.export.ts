261ed8d80953fc4d7959a1f48fa13ae8d907ef6f
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-01-20 12:22:33 -0500

    chore: Refactored env loading in the SDK

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -13,7 +13,7 @@ import fs from 'fs';
 
 // Utils
 import 'tapioca-sdk'; // Use directly the un-compiled code, no need to wait for the tarball to be published.
-import SDK from 'tapioca-sdk';
+import { SDK, loadEnv } from 'tapioca-sdk';
 import { HttpNetworkConfig, HttpNetworkUserConfig } from 'hardhat/types';
 import { TAPIOCA_PROJECTS_NAME } from '@tapioca-sdk/api/config';
 
@@ -27,25 +27,8 @@ declare global {
     }
 }
 
-const networkArg = process.argv.findIndex((c) => c === '--network');
-let networkName = 'localhost';
-
-if (networkArg !== -1) {
-    networkName = process.argv[networkArg + 1]; // Get the network name
-} else {
-    console.log('[!] No network specified, using localhost as default.');
-}
-
-// dotenv loading can not load `export` env vars for some reasons
-const path = `.env/${networkName}.env`;
-
-if (fs.existsSync(path)) {
-    dotenv.config({ path });
-} else {
-    throw new Error(
-        '[-] env vars not loaded, please specify a network with --network <network> and create its file in .env/<network>.env',
-    );
-}
+// Load the env vars from the .env/<network>.env file. the <network> file name is the same as the network in hh `--network arbitrum_sepolia`
+loadEnv();
 
 type TNetwork = ReturnType<
     typeof SDK.API.utils.getSupportedChains
@@ -151,22 +134,3 @@ const config: HardhatUserConfig & { dodoc: any } = {
 };
 
 export default config;
-
-extendEnvironment((hre) => {
-    // remove hardhat core tasks
-    delete hre.tasks['gas-reporter:merge'];
-    delete hre.tasks['export-artifacts'];
-    delete hre.tasks['size-contracts'];
-    delete hre.tasks['init-foundry'];
-    delete hre.tasks.coverage;
-    delete hre.tasks.sourcify;
-    delete hre.tasks.accounts;
-    delete hre.tasks.flatten;
-    delete hre.tasks.deploy;
-    delete hre.tasks.export;
-    delete hre.tasks.export;
-    delete hre.tasks.check;
-    delete hre.tasks.test;
-    delete hre.tasks.node;
-    delete hre.tasks.run;
-});
