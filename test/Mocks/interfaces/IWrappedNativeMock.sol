122d2ce30e5aa2379e2721cc137475f4fb947139
0xWeiss <mweiss.eth@gmail.com>
2024-01-21 02:57:58 +0400

    WIP: Renamed Wrapped mock

---
+++ test/Mocks/interfaces/IWrappedNativeMock.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+
+interface IWrappedNativeMock  {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+}
