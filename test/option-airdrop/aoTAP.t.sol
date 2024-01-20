b393598334886c1445cbfc613001cc40414e11ea
0xWeiss <mweiss.eth@gmail.com>
2024-01-20 20:55:50 +0400

    WIP: Removed unnecessary imports

--- test/option-airdrop/aoTAP.t.sol
+++ test/option-airdrop/aoTAP.t.sol
@@ -2,28 +2,11 @@
 
 pragma solidity 0.8.22;
 
-// LZ
-import {SendParam, MessagingFee, MessagingReceipt, OFTReceipt} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/interfaces/IOFT.sol";
-import {OFTComposeMsgCodec} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTComposeMsgCodec.sol";
-import {OptionsBuilder} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
-import {OFTMsgCodec} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oft/libs/OFTMsgCodec.sol";
-import {BytesLib} from "@layerzerolabs/solidity-bytes-utils/contracts/BytesLib.sol";
-import {Origin} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
-
-import {IMessagingChannel} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessagingChannel.sol";
 
 // External
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-//Tapioca
-import {ITapOFTv2, LockTwTapPositionMsg, UnlockTwTapPositionMsg, LZSendParam, ERC20PermitStruct, ERC721PermitStruct, ERC20PermitApprovalMsg, ERC721PermitApprovalMsg, ClaimTwTapRewardsMsg, RemoteTransferMsg} from "@contracts/tokens/TapOFTv2/ITapOFTv2.sol";
-import {TapOFTv2Helper, PrepareLzCallData, PrepareLzCallReturn, ComposeMsgData} from "@contracts/tokens/TapOFTv2/extensions/TapOFTv2Helper.sol";
-import {TapOFTMsgCoder} from "@contracts/tokens/TapOFTv2/TapOFTMsgCoder.sol";
-import {TwTAP, Participation} from "@contracts/governance/twTAP.sol";
-import {TapOFTReceiver} from "@contracts/tokens/TapOFTv2/TapOFTReceiver.sol";
-import {TapOFTSender} from "@contracts/tokens/TapOFTv2/TapOFTSender.sol";
 
 import {MockToken} from "gitsub_tapioca-sdk/src/contracts/mocks/MockToken.sol";
 
@@ -49,9 +32,6 @@ import "forge-std/console.sol";
 contract aoTapTest is TapTestHelper, Errors {
     using stdStorage for StdStorage;
 
-    using OptionsBuilder for bytes;
-    using OFTMsgCodec for bytes32;
-    using OFTMsgCodec for bytes;
 
     AOTAP public aotap; //instance of AOTAP
 
