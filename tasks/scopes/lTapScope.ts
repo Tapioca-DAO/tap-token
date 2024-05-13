3fd6ea09ca070f7664f0548646e05b9c6f9084f3
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-13 13:06:03 -0400

    feat(`ltap scope`): New `ltap_mintLtapMock__task`

--- tasks/scopes/lTapScope.ts
+++ tasks/scopes/lTapScope.ts
@@ -2,6 +2,7 @@ import { scope } from 'hardhat/config';
 import { TAP_TASK } from 'tapioca-sdk';
 import { setLockedUntilOnLtap__task } from 'tasks/exec/ltap/23-ltap-setLockedUntil';
 import { ltap__deployMock__task } from 'tasks/exec/ltap/ltap_deployMock';
+import { ltap_mintLtapMock__task } from 'tasks/exec/ltap/ltap_mintLtapMock';
 import { ltap_openRedemptions__task } from 'tasks/exec/ltap/ltap_openRedemptions';
 
 const lTapScope = scope('ltap', 'LockedTap setter tasks');
@@ -21,6 +22,15 @@ TAP_TASK(
         'Deploy a mock LTAP contract.',
         ltap__deployMock__task,
     ),
+);
+TAP_TASK(
+    lTapScope
+        .task(
+            'mintMock',
+            'Mint LTAP tokens to an address.',
+            ltap_mintLtapMock__task,
+        )
+        .addParam('userFile', 'Path to the JSON file with users and amounts'),
 );
 
 // --- LTAP
