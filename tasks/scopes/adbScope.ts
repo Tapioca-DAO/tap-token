59b744a916a45cd1efd8cab8b701ecf3094f6412
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-12 23:19:47 -0400

    chore(`adb tasks`): New `adb_setPhase2Roots__task`

--- tasks/scopes/adbScope.ts
+++ tasks/scopes/adbScope.ts
@@ -1,4 +1,5 @@
 import { scope } from 'hardhat/config';
+import { TAP_TASK } from 'tapioca-sdk';
 import { setTapOracle__task } from 'tasks/exec/adb/07-ab-setTapOracle';
 import { setPhase2MerkleRoots__task } from 'tasks/exec/adb/08-ab-setPhase2MerkleRoots';
 import { registerUserForPhase__task } from 'tasks/exec/adb/09-ab-registerUserForPhase';
@@ -6,8 +7,20 @@ import { setPaymentTokenOnAB__task } from 'tasks/exec/adb/10-ab-setPaymentToken'
 import { setPaymentTokenBeneficiaryAB__task } from 'tasks/exec/adb/11-ab-setPaymentTokenBeneficiary';
 import { collectPaymentTokensOnAB__task } from 'tasks/exec/adb/12-ab-collectPaymentTokens';
 import { daoRecoverTAPFromAB__task } from 'tasks/exec/adb/13-ab-daoRecoverTAP';
+import { adb_setPhase2Roots__task } from 'tasks/exec/adb/adb_registerUserForPhase';
 
 const adbScope = scope('adb', 'AirdropBroker setter tasks');
+
+TAP_TASK(
+    adbScope
+        .task(
+            'registerUserForPhase',
+            'Register users for phase 1 or 4 on AirdropBroker. A JSON file with users and amounts is required.',
+            adb_setPhase2Roots__task,
+        )
+        .addParam('phase', 'Phase number')
+        .addParam('userFile', 'Path to the JSON file with users and amounts'),
+);
 
 adbScope.task(
     'setTapOracleOnAB',
@@ -22,7 +35,7 @@ adbScope.task(
 );
 
 adbScope.task(
-    'registerUserForPhase',
+    'registerUserForPhase__deprecated',
     'Register user on AirdropBroker',
     registerUserForPhase__task,
 );
