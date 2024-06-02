4400463e4cebfe59d38c589c75ac1821beb278e6
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-06-02 11:46:08 -0400

    patch(`deployPreLbpStack__task`): Checking for `isHostChain`

--- tasks/deploy/1-deployPreLbpStack.ts
+++ tasks/deploy/1-deployPreLbpStack.ts
@@ -24,9 +24,23 @@ export const deployPreLbpStack__task = async (
 };
 
 async function tapiocaDeployTask(params: TTapiocaDeployerVmPass<object>) {
-    const { hre, VM, tapiocaMulticallAddr, taskArgs, isTestnet } = params;
+    const {
+        hre,
+        VM,
+        tapiocaMulticallAddr,
+        taskArgs,
+        isTestnet,
+        isHostChain,
+        isSideChain,
+    } = params;
     const { tag } = taskArgs;
     const owner = tapiocaMulticallAddr;
 
-    VM.add(await buildLTap(hre, DEPLOYMENT_NAMES.LTAP, [owner, owner], []));
+    if (isHostChain) {
+        VM.add(await buildLTap(hre, DEPLOYMENT_NAMES.LTAP, [owner, owner], []));
+    } else {
+        console.log(
+            '[+] Skipping LTAP deployment, current chain is not host chain.',
+        );
+    }
 }
