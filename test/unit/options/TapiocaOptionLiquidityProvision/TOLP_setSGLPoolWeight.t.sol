690e92c2d1d7187a01abcfa6df8860df5932b450
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-07-05 11:25:27 -0400

    test(`TOLP`): Add `TOLP_setSGLPoolWeight()` BTT

---
+++ test/unit/options/TapiocaOptionLiquidityProvision/TOLP_setSGLPoolWeight.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {TolpBaseTest, IERC20} from "./TolpBaseTest.t.sol";
+
+contract TOLP_setSGLPoolWeight is TolpBaseTest {
+    function test_RevertWhen_AssetIdIs0() external {
+        // it should revert
+        vm.startPrank(adminAddr);
+        vm.expectRevert(NotRegistered.selector);
+        tolp.setSGLPoolWeight(IERC20(address(0x1)), 1);
+    }
+
+    function test_ShouldSetTheWeightOfThePool() external registerSingularityPool {
+        // it should set the weight of the pool
+        vm.startPrank(adminAddr);
+        tolp.setSGLPoolWeight(IERC20(address(0x1)), 3);
+
+        (,, uint256 poolWeight,) = tolp.activeSingularities(IERC20(address(0x1)));
+        assertEq(poolWeight, 3, "TOLP_setSGLPoolWeight: Invalid weight");
+        //     and also update the total weights
+        assertEq(tolp.totalSingularityPoolWeights(), 7, "TOLP_setSGLPoolWeight: Invalid total weight");
+    }
+}
