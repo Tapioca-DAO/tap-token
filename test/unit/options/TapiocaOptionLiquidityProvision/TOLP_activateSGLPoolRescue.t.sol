f28e74634f6512c44d5299d008d637b396143a8d
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-07-08 14:20:13 -0400

    test(`TOLP`): Add `TOLP_activateSGLPoolRescue` BTT

---
+++ test/unit/options/TapiocaOptionLiquidityProvision/TOLP_activateSGLPoolRescue.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.22;
+
+import {TolpBaseTest, IERC20, SingularityPool} from "./TolpBaseTest.t.sol";
+
+contract TOLP_activateSGLPoolRescue is TolpBaseTest {
+    function test_ShouldSetRescueToTrue() external registerSingularityPool {
+        // it should set rescue to true
+        vm.startPrank(adminAddr);
+
+        tolp.requestSglPoolRescue(1);
+        vm.warp(block.timestamp + tolp.rescueCooldown());
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+        (,,, bool rescue) = tolp.activeSingularities(IERC20(address(0x1)));
+        assertEq(rescue, true, "TOLP_activateSGLPoolRescue: Invalid rescue");
+    }
+
+    function test_RevertWhen_SglAssetIdIs0() external {
+        // it should revert
+        vm.startPrank(adminAddr);
+
+        vm.expectRevert(NotRegistered.selector);
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+    }
+
+    function test_RevertWhen_SglRescueIsTrue() external registerSingularityPool {
+        // it should revert
+        vm.startPrank(adminAddr);
+
+        tolp.requestSglPoolRescue(1);
+        vm.warp(block.timestamp + tolp.rescueCooldown());
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+        vm.expectRevert(AlreadyActive.selector);
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+    }
+
+    function test_RevertWhen_SglRescueRequestIs0() external registerSingularityPool {
+        // it should revert
+        vm.startPrank(adminAddr);
+
+        vm.expectRevert(NotActive.selector);
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+    }
+
+    function test_RevertWhen_RescueCooldownNotMet() external registerSingularityPool {
+        // it should revert
+        vm.startPrank(adminAddr);
+
+        tolp.requestSglPoolRescue(1);
+        vm.expectRevert(RescueCooldownNotReached.selector);
+        tolp.activateSGLPoolRescue(IERC20(address(0x1)));
+        
+    }
+}
