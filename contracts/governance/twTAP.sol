e63b8ff0248e55871688bbb01f1f007483394997
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-07-23 10:30:38 -0400

    fix(`twAML`): Reset `cumulative` on participate if below `EPOCH_DURATION` `CU-86du6f2jh`
    https://github.com/Enigma-Dark/Tapioca-Engagement-June/issues/5

--- contracts/governance/twTAP.sol
+++ contracts/governance/twTAP.sol
@@ -427,6 +427,9 @@ contract TwTAP is
                     }
 
                     pool.cumulative -= (pool.averageMagnitude * aMagnitudeMultiplier / MULTIPLIER_PRECISION);
+                    if (pool.cumulative < EPOCH_DURATION) {
+                        pool.cumulative = EPOCH_DURATION;
+                    }
                 } else {
                     pool.cumulative = EPOCH_DURATION;
                 }
